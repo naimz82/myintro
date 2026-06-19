c1f56cf748cb76ca76b68d806c5e4fbfdc49c23b
Naim Zulkipli <naim@amz.com.my>
2026-06-19 15:54:36 +0800

    Rearranged nav menu code

--- js/main.js
+++ js/main.js
@@ -1,21 +1,2 @@
-let toggler = document.getElementById('toggler');
-let searchContainer = document.querySelector('.search-container');
-let icon = toggler.querySelector('i');
-
-toggler.addEventListener('click', function(){
-    
-    //toggle the 'open' class of the search container
-    searchContainer.classList.toggle('open');
-    
-    //change icon to chevron-left or chevron-right
-    if (searchContainer.classList.contains('open')) {
-        icon.classList.remove('fa-chevron-right');
-        icon.classList.add('fa-chevron-left');
-    } else {
-        icon.classList.remove('fa-chevron-left');
-        icon.classList.add('fa-chevron-right');
-    }
-
-});
 
 
